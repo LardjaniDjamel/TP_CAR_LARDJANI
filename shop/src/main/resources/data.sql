6af27c8cb010b2653d0655ae19ab21db77a5980b
Lardjani Djamel <djamel.lardjani.etu@univ-lille.fr>
2021-03-08 17:51:45 +0100

    rendu TP2

--- shop/src/main/resources/data.sql
+++ shop/src/main/resources/data.sql
@@ -1,12 +1,13 @@
 insert into Client ( id, nom ,prenom, adresse, email,numero, mot_de_passe) values (1,'djamel' ,'lard','1 rue','s@gmail','0566','sgdh');
 
-insert into Produit (id,  nom,price, quantite,  categorie, image) values  (1, 'HUAWEI ' , 570, 1, 'MateBook', 'hua.jpg');
-insert into Produit (id,  nom,price, quantite,  categorie, image) values  (2, 'Acer' , 790, 1, 'Gamer', 'Acer.jpg');
-insert into Produit (id,  nom,price, quantite,  categorie, image) values  (3, 'HP ' , 499, 1, 'Pavilion ', 'hpp.jpg');
-insert into Produit (id,  nom,price, quantite,  categorie, image) values  (4, 'Asus' , 799, 1, 'ZenBook', 'asus.jpg');
-insert into Produit (id,  nom,price, quantite,  categorie, image) values  (5, 'HP ProBook' , 329, 1, 'ProBook', 'hp.jpg');
-insert into Produit (id,  nom,price, quantite,  categorie, image) values  (6, 'Dell' , 650, 1, 'Gamer', 'Dell.jpg');
 
+insert into Produit (id,  nom,price, quantite,  categorie, image) values  (1, 'Asus' , 799, 1, 'ZenBook', 'asus.jpg');
+insert into Produit (id,  nom,price, quantite,  categorie, image) values  (2, 'HP ProBook' , 329, 1, 'ProBook', 'hp.jpg');
+insert into Produit (id,  nom,price, quantite,  categorie, image) values  (3, 'Dell' , 650, 1, 'Gamer', 'Dell.jpg');
+
+insert into Produit (id,  nom,price, quantite,  categorie, image) values  (4, 'HUAWEI ' , 570, 1, 'MateBook', 'hua.jpg');
+insert into Produit (id,  nom,price, quantite,  categorie, image) values  (5, 'Acer' , 790, 1, 'Gamer', 'Acer.jpg');
+insert into Produit (id,  nom,price, quantite,  categorie, image) values  (6, 'HP ' , 499, 1, 'Pavilion ', 'hpp.jpg');
 
 insert into Produit (id,  nom,price, quantite,  categorie, image) values  (7, 'Asus' , 799, 1, 'ZenBook', 'asus.jpg');
 insert into Produit (id,  nom,price, quantite,  categorie, image) values  (8, 'HP ProBook' , 329, 1, 'ProBook', 'hp.jpg');
